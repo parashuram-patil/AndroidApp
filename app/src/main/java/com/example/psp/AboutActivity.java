51fe8cdaa0b83d26887c0d6ea49ad6960b55f23d
parashuram-patil <parasharam_patil@ymail.com>
2019-08-04 13:04:03 +0530

    added navigation activities

---
+++ app/src/main/java/com/example/psp/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.example.psp;
+
+import android.os.Bundle;
+
+import com.example.psp.base.BaseActivity;
+
+public class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/example/psp/base/BaseActivity.java
+++ app/src/main/java/com/example/psp/base/BaseActivity.java
@@ -1,22 +1,32 @@
 package com.example.psp.base;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.psp.AboutActivity;
+import com.example.psp.MainActivity;
+import com.example.psp.NotificationActivity;
 import com.example.psp.R;
 import com.example.psp.drawer.DrawerNavItem;
 
 import java.util.ArrayList;
 
 public class BaseActivity extends AppCompatActivity {
+
+    ListView listView;
+    RelativeLayout mDrawerPane;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    ArrayList<DrawerNavItem> navItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +38,62 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     public void setContentView(int layoutResID) {
-        DrawerLayout drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
         FrameLayout activityContainer = (FrameLayout) drawerLayout.findViewById(R.id.activityContent);
         getLayoutInflater().inflate(layoutResID, activityContainer, true);
         super.setContentView(drawerLayout);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.syncState();
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
         populateDrawerItems();
     }
 
     private void populateDrawerItems() {
-        ArrayList<DrawerNavItem> mNavItems = new ArrayList<DrawerNavItem>();
-        mNavItems.add(new DrawerNavItem("Home", "Meetup destination", R.drawable.ic_launcher));
-        mNavItems.add(new DrawerNavItem("Preferences", "Change your preferences", R.drawable.ic_launcher));
-        mNavItems.add(new DrawerNavItem("About", "Get to know about us", R.drawable.ic_launcher));
+        navItems = new ArrayList<DrawerNavItem>();
+        navItems.add(new DrawerNavItem("Home", "Starting point", R.drawable.ic_launcher));
+        navItems.add(new DrawerNavItem("Notification", "Your all notifications", R.drawable.ic_launcher));
+        navItems.add(new DrawerNavItem("About", "About this App", R.drawable.ic_launcher));
 
-        ListView mDrawerList = (ListView) findViewById(R.id.navList);
-        DrawerListAdapter adapter = new DrawerListAdapter(this, mNavItems);
-        mDrawerList.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.navList);
+        DrawerListAdapter adapter = new DrawerListAdapter(this, navItems);
+        listView.setAdapter(adapter);
 
-        RelativeLayout mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
-
-        mDrawerList.setOnItemClickListener((parent, view, position, id) -> {
-            Toast.makeText(getBaseContext(), "Nav " + position + "Selected", Toast.LENGTH_LONG);
-            //selectItemFromDrawer(position);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            selectItemFromDrawer(position);
         });
+
+    }
+
+    private void selectItemFromDrawer(int position) {
+
+        switch (position) {
+            case 1 :
+                final Intent mainIntent = new Intent(this, MainActivity.class);
+                startActivity(mainIntent);
+                break;
+            case 2:
+                final Intent notificationIntent = new Intent(this, NotificationActivity.class);
+                startActivity(notificationIntent);
+                break;
+            case 3:
+                final Intent aboutIntent = new Intent(this, AboutActivity.class);
+                startActivity(aboutIntent);
+                break;
+        }
+
+        listView.setItemChecked(position, true);
+        setTitle(navItems.get(position).getmTitle());
+        drawerLayout.closeDrawer(mDrawerPane);
+
     }
 }
