03074efe98382b0a925a7f30fdc34ccd5f726719
parashuram-patil <parasharam_patil@ymail.com>
2019-08-28 10:28:30 +0530

    Bug Fix: wrong values from pending intent

--- app/src/main/java/com/example/psp/util/Util.java
+++ app/src/main/java/com/example/psp/util/Util.java
@@ -185,7 +185,7 @@ public class Util {
         if (clazz != null) {
             Intent intent = new Intent(context, clazz);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             builder.setContentIntent(pendingIntent);
             builder.setAutoCancel(true);
         }
