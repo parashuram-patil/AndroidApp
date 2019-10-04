e39a8a40d5eac6c7fe5906928a6b6e601d658add
parashuram-patil <parasharam_patil@ymail.com>
2019-10-04 15:11:46 +0530

    Showing App versionin with git Integration

--- app/src/main/java/com/example/psp/AboutActivity.java
+++ app/src/main/java/com/example/psp/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.example.psp;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.example.psp.base.BaseActivity;
 import com.example.psp.constants.Constants;
@@ -12,5 +15,15 @@ public class AboutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setTitle(Constants.TITLE_ABOUT);
+
+        TextView versionInfo =  findViewById(R.id.versionInfo);
+        versionInfo.setText("Version " + BuildConfig.VERSION_NAME);
+
+        TextView textView =(TextView)findViewById(R.id.licence);
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        String url = "https://github.com/parshuram-patil/AndroidApp/blob/master/README.md";
+        String text = "<a href='" + url + "'>" + getString(R.string.more_information) + "</a>";
+        textView.setText(Html.fromHtml(text, 0));
     }
 }
